8a5db99cdcc1cc738f4046b441a3f34f9a032200
Alain Papazoglou <alain@eliga.fr>
2016-09-05 15:19:31 +0200

    local.json via ENV

--- Dockerfile
+++ Dockerfile
@@ -19,10 +19,10 @@ RUN rm /etc/supervisor/conf.d/supervisord-mysqld.conf
 
 # Enabled mod rewrite for phabricator
 RUN a2enmod rewrite
- 
+
 # Set password to 'admin'
 RUN printf admin\\nadmin\\n | passwd
- 
+
 COPY ./startup.sh /opt/startup.sh
 COPY ./update.sh /opt/update.sh
 RUN chmod +x /opt/startup.sh
@@ -30,7 +30,8 @@ RUN chmod +x /opt/update.sh
 
 COPY php.ini /etc/php5/apache2/conf.d/php.ini
 COPY preamble.php /opt/preamble.php
-COPY ssmtp.conf /etc/ssmtp/ssmtp.conf
+COPY local.json /opt/local.json
+COPY ssmtp.conf /opt/ssmtp.conf
 
 COPY phabricator.conf /etc/apache2/sites-available/phabricator.conf
 RUN rm -f /etc/apache2/sites-enabled/000-default.conf
@@ -39,5 +40,4 @@ RUN ln -s /etc/apache2/sites-available/phabricator.conf /etc/apache2/sites-enabl
 RUN ulimit -c 10000
 
 EXPOSE 80
-CMD ["/usr/bin/supervisord"] 
-
+CMD ["/usr/bin/supervisord"]
--- startup.sh
+++ startup.sh
@@ -5,20 +5,24 @@ git clone git://github.com/facebook/libphutil.git
 git clone git://github.com/facebook/arcanist.git
 git clone git://github.com/facebook/phabricator.git
 
-sed -e "s/{{HTTPS}}/${HTTPS}/g" -i /opt/preamble.php
+sed -e "s/{{HTTPS}}/${HTTPS}/g" \
+    -i /opt/preamble.php
 cp /opt/preamble.php /opt/phabricator/support/
 
-/opt/phabricator/bin/config set mysql.host ${MYSQL_HOST}
-/opt/phabricator/bin/config set mysql.port ${MYSQL_PORT}
-/opt/phabricator/bin/config set mysql.user ${MYSQL_USER}
-/opt/phabricator/bin/config set mysql.pass ${MYSQL_PASS}
-/opt/phabricator/bin/config set phabricator.base-uri ${PHAB_URI}
+sed -e "s/{{MYSQL_HOST}}/${MYSQL_HOST}/g" \
+    -e "s/{{MYSQL_PORT}}/${MYSQL_PORT}/g" \
+    -e "s/{{MYSQL_USER}}/${MYSQL_USER}/g" \
+    -e "s/{{MYSQL_PASS}}/${MYSQL_PASS}/g" \
+    -e "s/{{PHAB_URI}}/${PHAB_URI//\//\\/}/g" \
+    -i /opt/local.json
+cp /opt/local.json /opt/phabricator/conf/local/local.json
 
 sed -e "s/{{SSMTP_EMAIL}}/${SSMTP_EMAIL}/g" \
     -e "s/{{SSMTP_SERVER}}/${SSMTP_SERVER}/g" \
     -e "s/{{SSMTP_PASS}}/${SSMTP_PASS}/g" \
     -e "s/{{PHAB_URI}}/${PHAB_URI//\//\\/}/g" \
-    -i /etc/ssmtp/ssmtp.conf
+    -i /opt/ssmtp.conf
+cp /opt/ssmtp.conf /etc/ssmtp/ssmtp.conf
 
 cd /opt/phabricator && ./bin/storage upgrade --force
 cd /opt/phabricator && ./bin/phd restart
